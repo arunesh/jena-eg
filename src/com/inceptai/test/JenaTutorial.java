e491b6eb68c66274268f0000395abe0b0e6fb14a
Arunesh Mishra <aruneshm@gmail.com>
2017-10-25 10:49:13 -0700

    add print

--- src/com/inceptai/test/JenaTutorial.java
+++ src/com/inceptai/test/JenaTutorial.java
@@ -3,6 +3,8 @@ package com.inceptai.test;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.vocabulary.*;
 
+import javax.swing.plaf.nimbus.State;
+
 public class JenaTutorial {
     public static String personURI = "http://sanmateo/jack_britt";
     public static String givenName = "Jack";
@@ -19,5 +21,25 @@ public class JenaTutorial {
                 .addProperty(VCARD.Given, givenName)
                 .addProperty(VCARD.Family, familyName);
         resource.addProperty(VCARD.N, blankResource);
+        dumpModel(model);
+    }
+
+    private static void dumpModel(Model model) {
+        StmtIterator iter = model.listStatements();
+
+        while (iter.hasNext()) {
+            Statement stmt = iter.nextStatement();
+            Resource res = stmt.getSubject();
+            Property predicate = stmt.getPredicate();
+            RDFNode rdfNode = stmt.getObject();
+            System.out.print("Resource: " + res.toString());
+            System.out.print("Property: " + predicate.toString());
+            if (rdfNode instanceof Resource) {
+                System.out.print (" Object resource: " + rdfNode.toString());
+            } else {
+                System.out.print( "Literal resource: " + rdfNode.toString());
+            }
+            System.out.println(".");
+        }
     }
 }

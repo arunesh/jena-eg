c7946580fb364bf8736c845807beb392e753e69b
Arunesh Mishra <aruneshm@gmail.com>
2017-10-26 14:51:14 -0700

    jena learnings 2

--- src/com/inceptai/test/JenaTutorial.java
+++ src/com/inceptai/test/JenaTutorial.java
@@ -14,16 +14,11 @@ public class JenaTutorial {
     public static String fullName = givenName + " " + familyName;
 
     public static void main(String args[]) {
-        Model model = ModelFactory.createDefaultModel();
 
-        Resource resource = model.createResource(personURI);
-        resource.addProperty(VCARD.FN, fullName);
+        Model model = loadExample1();
+        Model model2 = loadExample2();
+        // dumpModel(model);
 
-        Resource blankResource = model.createResource()
-                .addProperty(VCARD.Given, givenName)
-                .addProperty(VCARD.Family, familyName);
-        resource.addProperty(VCARD.N, blankResource);
-        dumpModel(model);
     }
 
     private static void dumpModel(Model model) {
@@ -51,9 +46,9 @@ public class JenaTutorial {
         model.write(System.out, "RDF/XML-ABBREV");
         System.out.println("N-TRIPLES");
         model.write(System.out, "N-TRIPLES");
-        readFromFile("vc-db-1.rdf");
+        // readFromFile("vc-db-1.rdf");
+        loadExample2();
     }
-
 
     private static Model readFromFile(String filename) {
         Model model = ModelFactory.createDefaultModel();
@@ -66,5 +61,40 @@ public class JenaTutorial {
         System.out.println("Dumping model read from file: " + filename);
         model.write(System.out);
         return model;
+    }
+
+    private static Model loadExample1() {
+        Model model = ModelFactory.createDefaultModel();
+
+        Resource resource = model.createResource(personURI);
+        resource.addProperty(VCARD.FN, fullName);
+
+        Resource blankResource = model.createResource()
+                .addProperty(VCARD.Given, givenName)
+                .addProperty(VCARD.Family, familyName);
+        resource.addProperty(VCARD.N, blankResource);
+        return model;
+    }
+
+    private static Model loadExample2() {
+        Model m = ModelFactory.createDefaultModel();
+        String nsA = "http://somewhere/else#";
+        String nsB = "http://nowhere/else#";
+        Resource root = m.createResource( nsA + "root" );
+        Property P = m.createProperty( nsA + "P" );
+        Property Q = m.createProperty( nsB + "Q" );
+        Resource x = m.createResource( nsA + "x" );
+        Resource y = m.createResource( nsA + "y" );
+        Resource z = m.createResource( nsA + "z" );
+        m.add( root, P, x ).add( root, P, y ).add( y, Q, z );
+        System.out.println( "# -- no special prefixes defined" );
+        m.write( System.out );
+        System.out.println( "# -- nsA defined" );
+        m.setNsPrefix( "nsA", nsA );
+        m.write( System.out );
+        System.out.println( "# -- nsA and cat defined" );
+        m.setNsPrefix( "cat", nsB );
+        m.write( System.out );
+        return m;
     }
 }
